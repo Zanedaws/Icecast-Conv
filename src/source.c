08fdfeead4f51b17283b935fe2fb0fb9018762d6
Zane <dawson50@purdue.edu>
2021-08-14 23:42:55 -0400

    add itype to source.c:1298

--- src/source.c
+++ src/source.c
@@ -1295,7 +1295,7 @@ _Itype_for_any(T) void *source_client_thread (void *arg : itype(_Ptr<T>))
 }
 
 
-void source_client_callback (client_t *client, void *arg)
+void source_client_callback (client_t *client : itype(_Ptr<client_t>), void *arg)
 {
     const char *agent;
     source_t *source = arg;
